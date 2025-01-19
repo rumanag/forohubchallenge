5db04a97a7d11b5fa87063d107c14f2f249e7b2d
Ricardo Umana <u3dide@gmail.com>
2025-01-19 15:04:14 -0500

    Iniciando ultima parte autorizacion

--- src/main/java/foro/hub/forohub/infra/security/SecurityConfigurations.java
+++ src/main/java/foro/hub/forohub/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package foro.hub.forohub.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,11 +19,29 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfigurations {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(csrf -> csrf.disable())
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        //.requestMatchers(HttpMethod.GET,"/api/health").permitAll()
+                        //.requestMatchers("/swagger-ui.html", "/v3/api-docs/", "/swagger-ui/").permitAll()
+                        .anyRequest().authenticated());
+                        //.logout(logout -> logout
+                        //.logoutUrl("/logout")
+                        //.logoutSuccessUrl("/login?logout")
+                        //.permitAll())
+                //.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity.csrf(csrf -> csrf.disable())
+//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .build();
+ //   }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
